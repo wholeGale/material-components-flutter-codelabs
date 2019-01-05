3164663819ec4909ffc8ec037617b1e1e495d908
xiangzhenbiao <xiangzhenbiao@fangdd.com>
2019-01-05 14:52:55 +0800

    利用material组建写登录界面

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -20,10 +20,18 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
+  // TODO: Add text editing controllers (101), TextEditingController: A controller for an editable text field.
+  final _usernameController = TextEditingController(
+    text: '向彪',
+  );
+  final _passwordController = TextEditingController(
+    text: '123456',
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //SafeArea : A widget that insets its child by sufficient padding to avoid intrusions by the operating system.
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 24.0),
@@ -40,8 +48,48 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+
+            // TODO: Add TextField widgets (101) 添加两个文本框，用于用户输入用户名和密码
+            //TextField : A text field lets the user enter text, either with hardware keyboard or with an onscreen keyboard.
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'UserName'
+              ),
+              controller: _usernameController,
+            ),
+            //spacer 间隔12.0
+            SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                filled: true,
+                labelText: 'Password'
+              ),
+              obscureText: true,
+              controller: _passwordController,
+            ),
+
+            // TODO: Add button bar (101) ButtonBar 把子项排列为一行。
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Cancel'),
+                  onPressed: (){
+//                  // TODO: Clear the text fields (101)
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                // TODO: Show the next page (101)
+                RaisedButton(
+                  child: Text('Next'),
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                )
+              ],
+            ),
+
           ],
         ),
       ),
